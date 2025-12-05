f54b2b49e09ce9960a7d500ec4773035bf962e0f
Shawn Evans <shawnevans328@gmail.com>
2025-12-04 20:57:07 -0500

    boundaries

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import pygame #type: ignore
 pygame.init()
 
 size = width, height = 1000, 800
-
 screen = pygame.display.set_mode(size)
 
 run = True
 
-player = pygame.Rect((100, 100, 100, 100))
+player = pygame.Rect((75, 75, 100, 100))
+
+vel = 5
 
 while run:
 
@@ -20,18 +21,20 @@ while run:
 
     key = pygame.key.get_pressed()
 
-    if key[pygame.K_LEFT] == True:
+    if key[pygame.K_LEFT] and player.x > 0:
         player.move_ip(-1, 0)
-    elif key[pygame.K_RIGHT] == True:
+    elif key[pygame.K_RIGHT] and player.x < width - 100:
         player.move_ip(1, 0)
-    elif key[pygame.K_UP] == True:
+    elif key[pygame.K_UP]:
         player.move_ip(0, -1)
-    elif key[pygame.K_DOWN] == True:
+    elif key[pygame.K_DOWN]:
         player.move_ip(0, 1)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    print(player.y)
 
     pygame.display.update()
 
